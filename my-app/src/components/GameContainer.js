55e6589aafd37c51b7e5efa3bf34fd5c649f57d1
Mark Hjuler Pedersen <73698747+BuilderMARK@users.noreply.github.com>
2023-12-29 22:33:43 +0100

    Man kan nu game

--- my-app/src/components/GameContainer.js
+++ my-app/src/components/GameContainer.js
@@ -2,44 +2,48 @@ import React, { useState } from "react";
 import {
   tileGenerator,
   create,
-  positions,
-  piece,
-  matchCheck,
-  invalidMovesCheck,
-  canMove,
-  isPositionWithinBoardBounds,
   move,
   findMatches,
 } from "../gameLogic/gameLogic";
+import { useSelector } from "react-redux";
 
 function GameOfThreeComponent() {
   const rows = 8;
   const cols = 8;
   const generator = tileGenerator();
   const [board, setBoard] = useState(() => create(generator, cols, rows));
-  const [matches, setMatches] = useState(() => findMatches(board));
+  const [selectedTiles, setSelectedTiles] = useState([]);
+  const highScore = useSelector((state) => state.highscore.highScore);
 
   const handleCellClick = (rowIndex, colIndex) => {
-    const newBoard = board.map((row, rIndex) => {
-      if (rIndex === rowIndex) {
-        const newRow = [...row];
-        newRow[colIndex] = (newRow[colIndex] + 1) % 3;
-        return newRow;
-      }
-      return row;
-    });
-    setBoard(newBoard);
+    const newSelectedTiles = [
+      ...selectedTiles,
+      { row: rowIndex, col: colIndex },
+    ];
+    if (newSelectedTiles.length === 2) {
+      const [first, second] = newSelectedTiles;
+      const newBoard = move(generator, board, first, second).board;
+      setBoard(newBoard);
+      setSelectedTiles([]);
+    } else {
+      setSelectedTiles(newSelectedTiles);
+    }
   };
 
   function startNewGame() {
-    console.log("New game");
     const newBoard = create(generator, cols, rows);
-    setBoard(create(generator, cols, rows));
-    setMatches(findMatches(newBoard));
+    setBoard(newBoard);
+    setSelectedTiles([]);
   }
 
-  function findMatchesOnBoard() {
-    setMatches(findMatches(board));
+  function submitscore() {
+    //submit score
+    alert(`Congratulations! You got ${highScore}!`);
+  }
+
+  function endgame() {
+    //end game
+    alert("Congratulations! You have won the game!");
   }
 
   return (
@@ -54,6 +58,12 @@ function GameOfThreeComponent() {
               height: "50px",
               border: "1px solid black",
               boxSizing: "border-box",
+              backgroundColor: selectedTiles.some(
+                (st) =>
+                  st.row === Math.floor(index / cols) && st.col === index % cols
+              )
+                ? "lightblue"
+                : "transparent",
             }}
             onClick={() =>
               handleCellClick(Math.floor(index / cols), index % cols)
@@ -63,7 +73,8 @@ function GameOfThreeComponent() {
           </div>
         ))}
         <button onClick={startNewGame}>Start new game</button>
-        <button onClick={findMatchesOnBoard}>Find match</button>
+        <button onClick={submitscore}>Submit Score</button>
+        <button onClick={endgame}>End game</button>
       </div>
     </div>
   );
--- my-app/src/components/HighScoreContainer.js
+++ my-app/src/components/HighScoreContainer.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function HighScoreContainer() {
-  // Ret stien til at matche highscoreReducer i din rootReducer
   const highScore = useSelector((state) => state.highscore.highScore);
 
   return (
